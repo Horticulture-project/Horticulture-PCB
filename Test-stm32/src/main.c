d1f0de31eb6a0de8723dcdc88f471cd614709f6a
Daniel N. Hansten <daniel.hansten@bouvet.no>
2020-11-21 16:42:59 +0100

    update code to remove Led0

--- Test-stm32/src/main.c
+++ Test-stm32/src/main.c
@@ -14,25 +14,17 @@
 #define STEP_DELAY   1000
 
 /* The devicetree node identifier for the "led0" alias. */
-#define LED0_NODE DT_ALIAS(led0)
 
-#if DT_HAS_NODE(LED0_NODE)
-#define LED0	DT_GPIO_LABEL(LED0_NODE, gpios)
-#define PIN	DT_GPIO_PIN(LED0_NODE, gpios)
-#if DT_PHA_HAS_CELL(LED0_NODE, gpios, flags)
-#define FLAGS	DT_GPIO_FLAGS(LED0_NODE, gpios)
-#endif
 #define MOT1A1 1
 #define MOT1A2 2
 #define MOT1B1 3
 #define MOT1B2 4
 
-#else
+
 /* A build error here means your board isn't set up to blink an LED. */
-#error "Unsupported board: led0 devicetree alias is not defined"
-#define LED0	""
+
 #define PIN	0
-#endif
+
 
 #ifndef FLAGS
 #define FLAGS	0
